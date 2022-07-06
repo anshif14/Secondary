0522e750a0653cd43826605b8fab175431b2695f
anshif14 <101887280+anshif14@users.noreply.github.com>
2022-07-06 13:38:29 +0100

    changed to floating point

--- multiply.py
+++ multiply.py
@@ -1,8 +1,8 @@
-num1=int(input("Enter the first number: "))
+num1=float(input("Enter first number: "))
 #input value for variable num1
-num2=int(input("Enter the second number: "))
+num2=float(input("Enter second number: "))
 #input value for variable num2
 mul=num1*num2;
 #perform multiplication operation
-print("the product of given numbers is: ",mul)
+print("the product of given numbers is:",mul)
 #display the product
