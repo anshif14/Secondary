2026797ab5316d3efb27db60c4df7f99d157285e
anshif14 <101887280+anshif14@users.noreply.github.com>
2022-07-06 13:36:26 +0100

    added more functions

--- multiply.c
+++ multiply.c
@@ -1,14 +1,21 @@
-#include <stdio.h>
-int main() {
-    double a, b, product;
-    printf("Enter two numbers: ");
-    scanf("%lf %lf", &a, &b);  
- 
-    // Calculating product
-    product = a * b;
+#include<stdio.h>
+int main()
+{
+  int add, sub, mul;
+  float div;
 
-    // %.2lf displays number up to 2 decimal point
-    printf("Product = %.2lf", product);
-    
-    return 0;
+  //addition
+  add = 5 + 2;
+
+  //subtraction
+  sub = 5 - 2;
+
+  //multiplication
+  mul = 5 * 2;
+
+  //division
+  div = 5 / (float)2;
+
+  printf("Add: %d, Sub: %d, Mul: %d, Div: %.2f", add,sub,mul,div);
+  return 0;
 }
